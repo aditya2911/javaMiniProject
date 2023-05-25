cc32c1b2900a309855e3b47daeabad2e6b10a88b
Aditya Salunke <adisalu15@gmail.com>
2023-05-25 10:23:28 +0530

    initial commit

---
+++ src/main/java/com/example/javaMiniProjectAditya/JavaMiniProjectAdityaApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaMiniProjectAditya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMiniProjectAdityaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMiniProjectAdityaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaMiniProjectAditya/JavaMiniProjectAdityaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaMiniProjectAditya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMiniProjectAdityaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
